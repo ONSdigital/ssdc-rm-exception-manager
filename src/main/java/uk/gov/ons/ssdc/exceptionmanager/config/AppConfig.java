0df8608e2e1a484a211344243e30a9b4a31505b6
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2021-11-16 08:50:01 +0000

    Update AppConfig.java

--- src/main/java/uk/gov/ons/ssdc/exceptionmanager/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ssdc/exceptionmanager/config/AppConfig.java
@@ -1,9 +1,8 @@
 package uk.gov.ons.ssdc.exceptionmanager.config;
 
+import com.godaddy.logging.LoggingConfigs;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
-
-import com.godaddy.logging.LoggingConfigs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
