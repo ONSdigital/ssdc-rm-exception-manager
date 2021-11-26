0b5fb4868904df92f059f6330349f1ab8b021224
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2021-11-26 11:51:46 +0000

    fix local logging (#17)

--- src/main/java/uk/gov/ons/ssdc/exceptionmanager/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ssdc/exceptionmanager/config/AppConfig.java
@@ -3,15 +3,22 @@ package uk.gov.ons.ssdc.exceptionmanager.config;
 import com.godaddy.logging.LoggingConfigs;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
+  @Value("$logging.profile")
+  private String loggingProfile;
+
   @PostConstruct
   public void init() {
-    LoggingConfigs.setCurrent(LoggingConfigs.getCurrent().useJson());
+    if (loggingProfile.equals("STRUCTURED")) {
+      LoggingConfigs.setCurrent(LoggingConfigs.getCurrent().useJson());
+    }
+
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 
